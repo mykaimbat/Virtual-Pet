1ce6225f9de70636bc4a87b6457faf5e69c77351
mykaimbat <myimb136@students.gdoe.net>
2026-05-04 07:29:25 +0000

    act 3 part a

--- VirtualPet.java
+++ VirtualPet.java
@@ -1,15 +1,15 @@
 public class VirtualPet
 {
-  /* enter code for instance variables, constructor
-     getName and toString methods
-  **/
- private int energy, happiness, weight, ageInYears, months;
- private String name;
+  private int energy;
+  private int happiness; 
+  private int weight; 
+  private int ageInYears;
+  private int months;
+  private String name;
 
-private static final int MINIMUM_WEIGHT = 5;
-private static final int MAXIMUM_LEVEL = 10;
+  private static final int MINIMUM_WEIGHT = 5;
 
- public VirtualPet(String petName)
+  public VirtualPet(String petName)
   {
     name = petName;
     energy = 0;
@@ -19,35 +19,16 @@ private static final int MAXIMUM_LEVEL = 10;
     ageInYears = 0;
   }
     
-  // Accessor Method   
   public String getName()
   {
     return name;
   }
      
-
-  
   public String toString()
   {
     return name + "'s Information:\nEnergy: " + energy + "\nHappiness: " 
-                    + happiness + "\nWeight: " + weight + " g\nAge: " 
-                    + months + " months and " + ageInYears + " years";    
+           + happiness + "\nWeight: " + weight + "g\nAge: " 
+           + months + " months and " + ageInYears + " years";    
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
